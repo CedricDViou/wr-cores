de8b8f692e6bcff65f46cb6e0030a7227546aa79
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2017-07-07 16:11:18 +0200

    wr_softpll_ng: added support for external DDMTD sampler

--- modules/wr_softpll_ng/xwr_softpll_ng.vhd
+++ modules/wr_softpll_ng/xwr_softpll_ng.vhd
@@ -78,6 +78,8 @@ entity xwr_softpll_ng is
     g_ref_clock_rate : integer := 125000000;
     g_ext_clock_rate : integer := 10000000;
 
+    g_use_sampled_ref_clocks : boolean := false;
+
     g_interface_mode      : t_wishbone_interface_mode      := PIPELINED;
     g_address_granularity : t_wishbone_address_granularity := BYTE
     );
@@ -91,6 +93,10 @@ entity xwr_softpll_ng is
 
 -- Reference inputs (i.e. the RX clocks recovered by the PHYs)
     clk_ref_i  : in std_logic_vector(g_num_ref_inputs-1 downto 0);
+
+-- Reference inputs (i.e. the RX clocks recovered by the PHYs), externally sampled
+    clk_ref_sampled_i : in std_logic_vector(g_num_ref_inputs-1 downto 0);
+    
 -- Feedback clocks (i.e. the outputs of the main or aux oscillator)
     clk_fb_i   : in std_logic_vector(g_num_outputs-1 downto 0);
 -- DMTD Offset clock
@@ -148,6 +154,7 @@ architecture wrapper of xwr_softpll_ng is
       g_divide_input_by_2    : boolean;
       g_ref_clock_rate       : integer;
       g_ext_clock_rate       : integer;
+      g_use_sampled_ref_clocks : boolean;
       g_interface_mode       : t_wishbone_interface_mode;
       g_address_granularity  : t_wishbone_address_granularity);
     port (
@@ -201,6 +208,7 @@ begin  -- behavioral
       g_with_ext_clock_input => g_with_ext_clock_input,
       g_reverse_dmtds        => g_reverse_dmtds,
       g_divide_input_by_2    => g_divide_input_by_2,
+      g_use_sampled_ref_clocks => g_use_sampled_ref_clocks,
       g_ref_clock_rate  => g_ref_clock_rate,
       g_ext_clock_rate  => g_ext_clock_rate
       )
