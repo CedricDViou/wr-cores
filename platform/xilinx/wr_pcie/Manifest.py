aaa6b05b7d64d49440d4faf539e62b67cdef7348
Pascal Bos <bosp@nikhef.nl>
2020-04-01 14:22:45 +0200

    moved axi4litebridge to general cores

--- platform/xilinx/wr_pcie/Manifest.py
+++ platform/xilinx/wr_pcie/Manifest.py
@@ -1,1 +1,1 @@
-files = [ "Pcie.tcl", "wb_axi4lite_bridge.vhd" ]
+files = [ "Pcie.tcl" ]
--- top/spec7_write_design/spec7_write_top.vhd
+++ top/spec7_write_design/spec7_write_top.vhd
@@ -192,8 +192,32 @@ entity spec7_write_top is
     rxn       : in  std_logic_vector(1 downto 0);
     rxp       : in  std_logic_vector(1 downto 0);
     txn       : out std_logic_vector(1 downto 0);
-    txp       : out std_logic_vector(1 downto 0)
+    txp       : out std_logic_vector(1 downto 0);
 
+    ---------------------------------------------------------------------------
+    -- Processing system
+    ---------------------------------------------------------------------------
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC
   );
 end entity spec7_write_top;
 
@@ -299,7 +323,32 @@ architecture top of spec7_write_top is
     );
   end component gen_10mhz;
 
-
+  component processing_system_wrapper is
+    port (
+      DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+      DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+      DDR_cas_n : inout STD_LOGIC;
+      DDR_ck_n : inout STD_LOGIC;
+      DDR_ck_p : inout STD_LOGIC;
+      DDR_cke : inout STD_LOGIC;
+      DDR_cs_n : inout STD_LOGIC;
+      DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+      DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+      DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+      DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+      DDR_odt : inout STD_LOGIC;
+      DDR_ras_n : inout STD_LOGIC;
+      DDR_reset_n : inout STD_LOGIC;
+      DDR_we_n : inout STD_LOGIC;
+      FIXED_IO_ddr_vrn : inout STD_LOGIC;
+      FIXED_IO_ddr_vrp : inout STD_LOGIC;
+      FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+      FIXED_IO_ps_clk : inout STD_LOGIC;
+      FIXED_IO_ps_porb : inout STD_LOGIC;
+      FIXED_IO_ps_srstb : inout STD_LOGIC;
+      gpio_rtl_0_tri_o : out STD_LOGIC_VECTOR ( 3 downto 0 )
+    );
+  end component processing_system_wrapper;
 begin  -- architecture top
 
   -- Never trigger PS_POR or PROGRAM_B
@@ -557,5 +606,30 @@ AXI2WB : xwb_axi4lite_bridge
     rst_n_i    => rst_ref_62m5_n,
     pulse_i    => wrc_pps_led,
     extended_o => led_pps);
-
+    
+  processing_system: processing_system_wrapper
+     port map (
+      DDR_addr(14 downto 0) => DDR_addr(14 downto 0),
+      DDR_ba(2 downto 0) => DDR_ba(2 downto 0),
+      DDR_cas_n => DDR_cas_n,
+      DDR_ck_n => DDR_ck_n,
+      DDR_ck_p => DDR_ck_p,
+      DDR_cke => DDR_cke,
+      DDR_cs_n => DDR_cs_n,
+      DDR_dm(3 downto 0) => DDR_dm(3 downto 0),
+      DDR_dq(31 downto 0) => DDR_dq(31 downto 0),
+      DDR_dqs_n(3 downto 0) => DDR_dqs_n(3 downto 0),
+      DDR_dqs_p(3 downto 0) => DDR_dqs_p(3 downto 0),
+      DDR_odt => DDR_odt,
+      DDR_ras_n => DDR_ras_n,
+      DDR_reset_n => DDR_reset_n,
+      DDR_we_n => DDR_we_n,
+      FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
+      FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
+      FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
+      FIXED_IO_ps_clk => FIXED_IO_ps_clk,
+      FIXED_IO_ps_porb => FIXED_IO_ps_porb,
+      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+      gpio_rtl_0_tri_o(3 downto 0) => open
+    );
 end architecture top;
