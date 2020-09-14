bf99cb6ef1e71a8dae243fd244bd4734c4eb8eef
Pascal Bos <bosp@nikhef.nl>
2020-09-14 11:37:49 +0200

    Added processing system to ref design.
    updated PCIe Memory map to fit WRPC-v5
    
    removed pcie version control

--- modules/wrc_core/wr_core.vhd
+++ modules/wrc_core/wr_core.vhd
@@ -44,7 +44,7 @@
 -------------------------------------------------------------------------------
 -- Memory map:
 --  0x00000000: I/D Memory
---  0x00020000: Peripheral interconnect
+--  0x00040000: Peripheral interconnect
 --      +0x000: Minic
 --      +0x100: Endpoint
 --      +0x200: Softpll
