96060dd4019becbd7105a2afb9bd0fc733377e18
A. Hahn <a.hahn@gsi.de>
2021-02-17 16:28:08 +0100

    platform/altera: removed generic arria10 phy from Manifest.py

--- platform/altera/wr_arria10_phy/Manifest.py
+++ platform/altera/wr_arria10_phy/Manifest.py
@@ -2,7 +2,6 @@ def __helper():
   dirs = []
   if syn_device[:4] == "10as":    dirs.extend(["wr_arria10_phy"])
   if syn_device[:7] == "10ax027": dirs.extend(["wr_arria10_scu4_phy"])
-  if syn_device[:7] == "10ax066": dirs.extend(["wr_arria10_phy"])
   if syn_device[:7] == "10ax115": dirs.extend(["wr_arria10_e3p1_phy"])
   return dirs
 
