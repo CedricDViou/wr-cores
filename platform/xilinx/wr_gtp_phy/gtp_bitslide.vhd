7a6d4c61f6f6272be81a15618f19282dc0ddcca7
Peter Jansweijer <peterj@nikhef.nl>
2023-01-13 11:03:05 +0100

    Revert gtp_bitslide.vhd SHA-1: c9cc5cab4a1b0868391d32726c017816f4749a62 back to version SHA-1: 8a4c3198c7562283970ff154895ca631cfa62fac SHA-1: c9cc5cab4a1b0868391d32726c017816f4749a62a slightly changed the behavior of gtp_rx_cdr_rst_o which breaks proper functionality. Artix-7 GTP failed to properly align with a matching bitslide value.

--- platform/xilinx/wr_gtp_phy/gtp_bitslide.vhd
+++ platform/xilinx/wr_gtp_phy/gtp_bitslide.vhd
@@ -6,7 +6,7 @@
 -- Author     : Tomasz Wlostowski
 -- Company    : CERN BE-CO-HT
 -- Created    : 2010-11-18
--- Last update: 2020-07-08
+-- Last update: 2020-07-03
 -- Platform   : FPGA-generic
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -87,16 +87,6 @@ end gtp_bitslide;
 architecture behavioral of gtp_bitslide is
 
 
-  function f_eval_link_down_threshold return integer is
-  begin
-    if(g_simulation /= 0) then
-      return 256;
-    else
-      return 10000; -- 10000 bytes without comma = link down
-    end if;
-  end f_eval_link_down_threshold;
-  
-  
 
   function f_eval_sync_detect_threshold
     return integer is
@@ -227,7 +217,6 @@ begin  -- behavioral
               state <= S_SLIDE;
             else
               state <= S_GOT_SYNC;
-              counter <= to_unsigned(0, counter'length);
             end if;
           end if;
 
@@ -235,15 +224,7 @@ begin  -- behavioral
           gtp_rx_slide_o <= '0';
           bitslide_o     <= std_logic_vector(cur_slide(4 downto 0));
           synced_o       <= '1';
-          if gtp_rx_comma_det_i = '1' then
-            counter <= to_unsigned(0, counter'length);
-          else
-            counter <= counter + 1;
-          end if;
-          
-          -- gtp_rx_byte_is_aligned_i = '0' or serdes_ready_i = '0' or 
-          if(counter = f_eval_link_down_threshold) then
-            report "serdes: link down" severity error;
+          if(gtp_rx_byte_is_aligned_i = '0' or serdes_ready_i = '0') then
             gtp_rx_cdr_rst_o <= '1';
             state            <= S_SYNC_LOST;
           end if;
