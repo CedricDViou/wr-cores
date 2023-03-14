514aec3684965f56e84599422ac6ee1a05ad0d1b
Cedric Viou <cedric.viou@obs-nancay.fr>
2023-03-14 20:18:46 +0000

    Fix required module list in Manifest

--- platform/altera/wr_arria10_phy/wr_arria10_idrogen_phy/Manifest.py
+++ platform/altera/wr_arria10_phy/wr_arria10_idrogen_phy/Manifest.py
@@ -1,5 +1,5 @@
 files = [
-  "gtp_bitslide.vhd",
+  "wr_arria10_idrogen_atx_pll/wr_arria10_idrogen_atx_pll.qsys",
   "wr_arria10_idrogen_cmu_pll/wr_arria10_idrogen_cmu_pll.qsys",
   "wr_arria10_idrogen_det_phy/wr_arria10_idrogen_det_phy.qsys",
   "wr_arria10_idrogen_rst_ctl/wr_arria10_idrogen_rst_ctl.qsys"
